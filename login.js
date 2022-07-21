d776a5e2ce7423a05018a7b47116fb100851e2aa
Ruslan_gg <107236487+Navielon@users.noreply.github.com>
2022-07-21 19:08:57 +0300

    upd

--- login.js
+++ login.js
@@ -1,36 +1,13 @@
-let switchCtn = document.querySelector('#switch-cnt');
-let switchC1 = document.querySelector('#switch-c1');
-let switchC2 = document.querySelector('#switch-c2');
-let switchCircle = document.querySelectorAll('.switch__circle');
-let switchBtn = document.querySelectorAll('.switch-btn');
-let aContainer = document.querySelector('#a-container');
-let bContainer = document.querySelector('#b-container');
-let allButtons = document.querySelectorAll('.submit');
+var card=document.getElementById("card");
+function openRegister(){
+   document.querySelector('.card-front').classList.add('hidden')
+   document.querySelector('.card-back').classList.remove('hidden')
+   //  card.style.transform="rotateY(-180deg)";
+   //  card.style.transform="rotate(360deg)";
+}
+function openLogin(){
+    card.style.transform="rotateY(0deg)";
+   document.querySelector('.card-front').classList.remove('hidden')
+   document.querySelector('.card-back').classList.add('hidden')
 
-let getButtons = e => e.preventDefault();
-
-let changeForm = e => {
-  switchCtn.classList.add('is-gx');
-  setTimeout(function () {
-    switchCtn.classList.remove('is-gx');
-  }, 1500);
-
-  switchCtn.classList.toggle('is-txr');
-  switchCircle[0].classList.toggle('is-txr');
-  switchCircle[1].classList.toggle('is-txr');
-
-  switchC1.classList.toggle('is-hidden');
-  switchC2.classList.toggle('is-hidden');
-  aContainer.classList.toggle('is-txl');
-  bContainer.classList.toggle('is-txl');
-  bContainer.classList.toggle('is-z200');
-};
-
-let mainF = e => {
-  for (var i = 0; i < allButtons.length; i++)
-    allButtons[i].addEventListener('click', getButtons);
-  for (var i = 0; i < switchBtn.length; i++)
-    switchBtn[i].addEventListener('click', changeForm);
-};
-
-window.addEventListener('load', mainF);
+}
